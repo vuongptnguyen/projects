8e3ed9f17a07f6b379dc721fec36d4e09d8304b8
Vuong Nguyen <nptvuong2912@gmail.com>
2016-04-07 10:49:37 -0500

    update password access to database

--- LoginSystem/common.php
+++ LoginSystem/common.php
@@ -1,8 +1,12 @@
 <?php 
-
+	//get password to database
+	$file = fopen("password.txt", "r") or die ("Cannot access to password text");
+	$passRead = fread($file,filesize("password.txt"));
+	fclose($file);
+	
     // These variables define the connection information for your MySQL database 
     $username = "vnguyen42"; 
-    $password = "vuongnguyen42"; 
+    $password = $passRead; 
     $host = "mysql.eecs.ku.edu"; 
     $dbname = "vnguyen42"; 
     
